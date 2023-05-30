8446a6c692ab2c09c407e41c6aa4ab0c29391a88
PatrykBombik <patryk.bombik@gmail.com>
2023-05-30 12:19:23 +0200

    fetch and myFetch custom implementation with XMLHTTPRequest (AJAX) object

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// const boxRef = document.querySelector('.box');
+const boxRef = document.querySelector('.box');
 //
 // let isFinished = false;
 // setTimeout(() => {
@@ -21,19 +21,72 @@
 //     }
 // }, 15000)
 
-const receipt = new Promise ((resolve, reject) => {
-    setTimeout(() => {
-        resolve('Kebab maj friend!');
-    }, 3001);
-    setTimeout(() => {
-        reject('No kebab, łonna fajt?');
-    }, 3000);
-});
+// const receipt = new Promise ((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve('Kebab maj friend!');
+//     }, 3001);
+//     setTimeout(() => {
+//         reject('No kebab, łonna fajt?');
+//     }, 3000);
+// });
+//
+// receipt
+//     .then ((msg) => {
+//         console.log(msg)
+//     })
+//     .catch ((msg) => {
+//         console.log(msg)
+//     });
 
-receipt
-    .then ((msg) => {
-        console.log(msg)
+// function move(top, left) {
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             boxRef.style.left = left;
+//             boxRef.style.top = top;
+//             resolve();
+//         }, 3000);
+//     });
+// }
+//
+// move(0, '500px')
+//     .then(() => move('500px', '500px'))
+//     .then(() => move('500px', 0))
+//     .then(() => move(0, 0));
+
+const url = 'https://api.nbp.pl/api/exchangerates/rates/a/usd/?format=json';
+
+// const request = fetch(url);
+//
+// request
+//     .then((response) => response.json())
+//     .then((data) => console.log(data.rates[0].mid))
+//     .catch(console.log);
+
+function myFetch(url) {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest()
+
+        xhr.onload = function () {
+            if (xhr.status >= 200 && xhr.status < 400) {
+                const response = JSON.parse(xhr.responseText)
+                resolve(response);
+            } else {
+                reject(xhr.status);
+            }
+        }
+
+        xhr.onerror = function () {
+            reject('Something is no yes!')
+        }
+
+        xhr.open('GET', url, true);
+        xhr.send()
     })
-    .catch ((msg) => {
-        console.log(msg)
-    });
+}
+
+const request = myFetch(url);
+
+request
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error))
+    .finally(() => {console.log('done!')})
